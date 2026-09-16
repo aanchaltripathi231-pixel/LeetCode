e8c68a4a159e7f1f719a2b2280ada8e07a6d32ad
aanchaltripathi231-pixel <aanchaltripathi231@gmail.com>
2026-09-16 18:55:01 +0530

    [1903] [Time Beats: 100.00%] [Memory Beats: 50.23%] - LeetPush

---
+++ 1903-Largest-Odd-Number-in-String.js
@@ -0,0 +1,9 @@
+var largestOddNumber = function(num) {
+    for (let i = num.length - 1; i >= 0; i--) {
+        if ((num[i] - '0') % 2 !== 0) {
+            return num.substring(0, i + 1);
+        }
+    }
+
+    return "";
+};
